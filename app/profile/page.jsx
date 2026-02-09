a5cbfdeec969cb86c304c04a8e8b55bac5a7788c
2029ijones-sudo <2029ijones@wfschools.org>
2026-02-09 15:36:23 -0600

    Update page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 // /app/profile/page.jsx
 'use client';
-import Profile from '../components/Profile';
+import Profile from '../../components/Profile';  // Changed from '../components/Profile'
 
 export default function ProfilePage() {
   return <Profile />;
